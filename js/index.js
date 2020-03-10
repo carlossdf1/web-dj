474aeeed3a16b4ac1b1fe5911e6d6acedd7fc338
carlossdf1 <carlos.sdf1@gmail.com>
2020-03-10 01:18:31 -0300

    Mejoras en javascript y css

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function redimensionar() {
     $(document).ready(function() {
         var height = $(HTMLBodyElement).height();
-        $('#fondo').height(height + 300);
-        $('#conten').height(height + 300);
+        $('#fondo').height(height + 130);
+        $('#conten').height(height + 130);
     });
 }
